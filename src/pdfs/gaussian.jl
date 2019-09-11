0cdc67d8f429b410dc1df9bc214c0079d58752bf
Niklas Heim <heim.niklas@gmail.com>
2019-09-11 19:17:16 +0200

    move unit/scalar/diag var to cgaussian

--- src/pdfs/gaussian.jl
+++ src/pdfs/gaussian.jl
@@ -1,29 +1,12 @@
 export Gaussian, CGaussian
 export mean, variance, mean_var, sample, loglikelihood, kld
 
-DiagVar = AbstractArray{T,1} where T
-ScalarVar = AbstractArray{T,0} where T
-UnitVar = Array{T,1} where T
-XVar = Union{DiagVar, ScalarVar, UnitVar}
-
-struct Gaussian{T,V<:XVar} <: AbstractPDF{T}
+struct Gaussian{T} <: AbstractPDF{T}
     μ::AbstractArray{T}
-    σ2::V
+    σ2::AbstractArray{T}
 end
 
-function Gaussian(μ::AbstractArray{T}, σ2::Number) where T 
-    p = Gaussian{T,ScalarVar}(μ, zeros(T))
-    p.σ2 .+ σ2
-    p
-end
-
-function Gaussian(μ::AbstractArray{T}, σ2::AbstractArray{T}) where T
-    @assert ndims(μ) == ndims(σ2) == 1
-    @assert size(μ) == size(σ2)
-    Gaussian{T,DiagVar}(μ, σ)
-end
-
-Gaussian(μ::AbstractArray{T}) where T = Gaussian{T,UnitVar}(μ, ones(T, size(μ,1)))
+Flux.@treelike Gaussian
 
 
 length(p::Gaussian) = size(p.μ, 1)
@@ -33,7 +16,8 @@ mean_var(p::Gaussian) = (p.μ, p.σ2)
 
 function sample(p::Gaussian{T}; batch=1) where T
     k = length(p)
-    p.μ .+ sqrt.(p.σ2) .* randn(T, k, batch)
+    μ, σ2 = mean_var(p)
+    μ .+ sqrt.(σ2) .* randn(T, k, batch)
 end
 
 function loglikelihood(p::Gaussian, x::AbstractArray)
@@ -41,27 +25,65 @@ function loglikelihood(p::Gaussian, x::AbstractArray)
     - (sum((x .- p.μ).^2 ./ p.σ2, dims=1) .+ sum(log.(p.σ2)) .+ k*log(2π)) ./ 2
 end
 
+abstract type XVar end
+struct DiagVar <: XVar end
+struct ScalarVar <: XVar end
+struct UnitVar <: XVar end
 
+struct CGaussian{T,V<:XVar} <: AbstractCPDF{T}
+    xlength::Int
+    zlength::Int
+    encoder
 
-struct CGaussian{T} <: AbstractCPDF{T}
-    μ
-    σ2::AbstractArray{T}
+    function CGaussian{T,V}(xlength, zlength, encoder) where T where V
+        ex = encoder(randn(T, zlength))
+        cg = new(xlength, zlength, encoder)
+        if V == UnitVar
+            size(ex) == (xlength,) ? cg : error("With UnitVar encoder must return samples of xlength")
+        elseif V == ScalarVar
+            size(ex) == (xlength+1,) ? cg : error("With ScalarVar encoder must return samples of xlength+1")
+        else
+            size(ex) == (xlength*2,) ? cg : error("With DiagVar encoder must return samples of xlength*2")
+        end
+    end
 end
 
-length(p::CGaussian) = size(p.σ2, 1)
-mean(p::CGaussian, z::AbstractArray) = p.μ(z)
-variance(p::CGaussian, z::AbstractArray) = p.σ2
-mean_var(p::CGaussian, z::AbstractArray) = (mean(p, z), variance(p, z))
+Flux.@treelike CGaussian
+
+
+xlength(p::CGaussian) = p.xlength
+zlength(p::CGaussian) = p.zlength
+length(p::CGaussian) = (p.xlength, p.zlength) # TODO: how to deal with this?
+
+function mean_var(p::CGaussian{T}, z::AbstractArray) where T
+    ex = p.encoder(z)
+    # TODO: replace with softplus_safe
+    return ex[1:p.xlength,:], softplus.(ex[p.xlength+1:end])
+end
+
+function mean_var(p::CGaussian{T,UnitVar}, z::AbstractArray) where T
+    μ = p.encoder(z)
+    return μ, I
+end
+
+mean(p::CGaussian, z::AbstractArray) = mean_var(p, z)[1]
+variance(p::CGaussian, z::AbstractArray) = mean_var(p, z)[2]
 
 function sample(p::CGaussian{T}, z::AbstractArray; batch=1) where T
     (μ, σ2) = mean_var(p, z)
-    k = length(p)
+    k = xlength(p)
     μ .+ sqrt.(σ2) .* randn(T, k, batch)
 end
 
+function sample(p::CGaussian{T,UnitVar}, z::AbstractArray; batch=1) where T
+    k = length(p)
+    mean(p) .+ randn(T, k, batch)
+end
+
+
 function loglikelihood(p::CGaussian, x::AbstractArray, z::AbstractArray)
     (μ, σ2) = mean_var(p, z)
-    k = length(p)
+    k = xlength(p)
     - (sum((x - μ).^2 ./ σ2, dims=1) .+ sum(log.(σ2)) .+ k*log(2π)) ./ 2
 end
 
@@ -82,17 +104,14 @@ function kld(p::CGaussian, q::Gaussian, z::AbstractArray)
 end
 
 
-Flux.@treelike Gaussian
-Flux.@treelike CGaussian
-
 function Base.show(io::IO, p::Gaussian{T}) where T
     msg = "Gaussian{$T}(μ=$(summary(p.μ)), σ2=$(summary(p.σ2)))"
     print(io, msg)
 end
 
-function Base.show(io::IO, p::CGaussian{T}) where T
-    μ = repr(p.μ)
-    μ = sizeof(μ)>50 ? "($(μ[1:47])...)" : μ
-    msg = "Gaussian{$T}(μ=$(μ), σ2=$(summary(p.σ2)))"
+function Base.show(io::IO, p::CGaussian{T,V}) where T where V
+    e = repr(p.encoder)
+    e = sizeof(e)>50 ? "($(e[1:47])...)" : e
+    msg = "CGaussian{$T,$V}(xlength=$(p.xlength), zlength=$(p.zlength), encoder=$e)"
     print(io, msg)
 end
