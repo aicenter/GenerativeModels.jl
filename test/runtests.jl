98b015eaa7876333a3a1579414bd33d7b7b88ac8
Niklas Heim <niklas.heim@aic.fel.cvut.cz>
2019-09-16 18:09:38 +0200

    try fixing seed to get reproducable tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,20 @@
 using Test
 using Logging
 using Parameters
+using Random
 
 using DifferentialEquations
 using DiffEqFlux
 
 using GenerativeModels
+
+# Fix seed for reconstruction error tests
+# TODO: does this have to be called in every @testset?
+Random.seed!(1)
+
+# set logging to debug to get more test output
+logger = ConsoleLogger(stdout, Logging.Info)
+global_logger(logger)
 
 # for testing of parameter change in training
 get_params(model) =  map(x->copy(Flux.Tracker.data(x)), collect(params(model)))
