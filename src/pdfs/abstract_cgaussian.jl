750c71667e922365500cb619d47301f5e832a6b7
Niklas Heim <niklas.heim@aic.fel.cvut.cz>
2019-09-16 11:25:49 +0200

    fix kld return dim

--- src/pdfs/abstract_cgaussian.jl
+++ src/pdfs/abstract_cgaussian.jl
@@ -20,5 +20,5 @@ function kld(p::AbstractCGaussian{T}, q::Gaussian{T}, z::AbstractArray) where T
     N = size(z, 2)
     (μ1, σ1) = mean_var(p, z)
     (μ2, σ2) = mean_var(q)
-    (sum(log.(σ2 ./ σ1)) + sum(σ1 ./ σ2) + sum((μ2 .- μ1).^2 ./ σ2)) / N
+    mean(log.(σ2 ./ σ1), dims=1) .+ mean(σ1 ./ σ2, dims=1) .+ mean((μ2 .- μ1).^2 ./ σ2, dims=1)
 end
