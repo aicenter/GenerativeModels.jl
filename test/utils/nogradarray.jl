8e277802c7aebd581b321e8fed24fb04914bb892
Niklas Heim <niklas.heim@aic.fel.cvut.cz>
2019-11-05 11:52:32 +0100

    add test that covers #49

--- test/utils/nogradarray.jl
+++ test/utils/nogradarray.jl
@@ -2,18 +2,41 @@
 
     @info "Testing NoGradArray"
 
-    x = NoGradArray(ones(3))
-    y = ones(3)
+    @testset "Params on CPU/GPU" begin
+        x = NoGradArray(ones(3))
+        y = ones(3)
 
-    p = Gaussian(x, y)
-    @test length(params(p)) == 1
+        p = Gaussian(x, y)
+        @test length(params(p)) == 1
 
-    g = gpu(p)
-    @test length(params(g)) == 1
-    @test rand(g) isa CuArray
+        g = gpu(p)
+        @test length(params(g)) == 1
+        @test rand(g) isa CuArray
 
-    c = cpu(g)
-    @test length(params(c)) == 1
-    @test rand(c) isa Array
+        c = cpu(g)
+        @test length(params(c)) == 1
+        @test rand(c) isa Array
+    end
+
+    @testset "Gradients" begin
+        enc = CMeanVarGaussian{Float32,DiagVar}(Dense(5,4))
+        dec = CMeanVarGaussian{Float32,ScalarVar}(Dense(2,6))
+        pri = Gaussian(NoGradArray(ones(Float32, 2)), ones(Float32, 2))
+        
+        vae = VAE(pri, enc, dec)
+        x = ones(5)
+        
+        vae = gpu(vae)
+        x = gpu(x)
+        
+        loss() = elbo(vae, x)
+        ps = params(vae)
+        @test length(ps) == 5
+        
+        gs = Zygote.gradient(loss, ps)
+        for p in ps
+            @test haskey(gs, p)
+        end
+    end
 
 end
