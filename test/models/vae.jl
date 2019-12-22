a291cee394f620cc1e9230f4aba915e16d91c9ca
Vít Škvára <vit.skvara@gmail.com>
2019-12-22 12:37:24 +0100

    vanilla vae (#76)
    Test VAE with `CMeanVarGaussian`

--- test/models/vae.jl
+++ test/models/vae.jl
@@ -7,38 +7,38 @@
         xlen = 4
         zlen = 2
         batch = 20
-        test_data = hcat(ones(T,xlen,Int(batch/2)), -ones(T,xlen,Int(batch/2))) |> gpu
+        test_data = randn(T, 4, batch)/100 .+ hcat(ones(T,xlen,Int(batch/2)), -ones(T,xlen,Int(batch/2))) |> gpu
     
-        enc = GenerativeModels.stack_layers([xlen, 10, 10, zlen], relu, Dense)
-        enc_dist = CMeanGaussian{DiagVar}(enc, NoGradArray(ones(T,zlen)))
+        enc = GenerativeModels.stack_layers([xlen, 4, zlen*2], relu, Dense)
+        enc_dist = CMeanVarGaussian{DiagVar}(enc)
     
-        dec = GenerativeModels.stack_layers([zlen, 10, 10, xlen], relu, Dense)
-        dec_dist = CMeanGaussian{DiagVar}(dec, NoGradArray(ones(T,xlen)))
+        dec = GenerativeModels.stack_layers([zlen, 4, xlen+1], relu, Dense)
+        dec_dist = CMeanVarGaussian{ScalarVar}(dec)
     
         model = VAE(zlen, enc_dist, dec_dist) |> gpu
     
-        loss = -elbo(model, test_data)
+        loss = - elbo(model, test_data)
         ps = params(model)
         @test length(ps) > 0
         @test isa(loss, Real)
     
         zs = rand(model.encoder, test_data)
         @test size(zs) == (zlen, batch)
-        xs = rand(model.decoder, zs)
+        xs = mean(model.decoder, zs)
         @test size(xs) == (xlen, batch)     
     
         # test training
         params_init = get_params(model)
         opt = ADAM()
-        data = [(test_data,) for i in 1:10000]
-        lossf(x) = -elbo(model, x, β=1e-3)
+        data = [(test_data,) for i in 1:2000]
+        lossf(x) = - elbo(model, x, β=1e0)
         Flux.train!(lossf, params(model), data, opt)
     
         @test all(param_change(params_init, model)) # did the params change?
-        zs = rand(model.encoder, test_data)
-        xs = mean(model.decoder, zs)
-        @debug maximum(test_data - xs)
-        @test all(abs.(test_data - xs) .< 0.2) # is the reconstruction ok?
+        nxs = mean(model.decoder, rand(model.encoder, test_data))
+        @debug maximum(test_data - nxs)
+        @test Flux.mse(test_data, nxs) < Flux.mse(test_data, xs)
+        # this seems to be a stable way to test reconstruction
     end
 
     @testset "Wasserstein VAE" begin
