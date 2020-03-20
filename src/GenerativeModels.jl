57f467d8236030215cecf1c9c2e37e11fd09fa40
Niklas Heim <heim.niklas@gmail.com>
2020-03-20 12:10:11 +0100

    gan fixes

--- src/GenerativeModels.jl
+++ src/GenerativeModels.jl
@@ -24,12 +24,12 @@ module GenerativeModels
     abstract type AbstractVAE <: AbstractGM end
     abstract type AbstractGAN <: AbstractGM end
 
-    # include(joinpath("utils", "flux_ode_decoder.jl"))
-    # include(joinpath("utils", "saveload.jl"))
+    include(joinpath("utils", "flux_ode_decoder.jl"))
+    include(joinpath("utils", "saveload.jl"))
     include(joinpath("utils", "utils.jl"))
 
     include(joinpath("models", "vae.jl"))
-    # include(joinpath("models", "rodent.jl"))
-    # include(joinpath("models", "gan.jl"))
+    include(joinpath("models", "rodent.jl"))
+    include(joinpath("models", "gan.jl"))
 
 end # module
